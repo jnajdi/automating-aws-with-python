e0a124b790b614d1b108cedf3960a3a89c66e078
Jihane Najdi <jihane.najdi@vt.edu>
2018-07-25 17:08:39 -0400

    Sync directory tree to s3 Bucket

--- webotron/webotron.py
+++ webotron/webotron.py
@@ -1,5 +1,7 @@
 import boto3
 import click
+import mimetypes
+from pathlib import Path
 
 from botocore.exceptions import ClientError
 
@@ -30,7 +32,7 @@ def list_bucket_objects(bucket):
 def setup_bucket(bucket):
     "Create and setup an S3 bucket"
     s3_bucket = None
-    
+
     try:
         s3_bucket = s3.create_bucket(Bucket=bucket)
         print(s3_bucket)
@@ -70,9 +72,36 @@ def setup_bucket(bucket):
             'IndexDocument': {
                 'Suffix': 'index.html'
             }})
-
+#
     return
 
+def upload_file(s3_bucket, path, key):
+    print(key)
+    content_type = mimetypes.guess_type(key)[0] or 'text/html'
+    print(content_type)
+
+    s3_bucket.upload_file(path, key, ExtraArgs={'ContentType' : content_type})
+
+@cli.command('sync')
+@click.argument('pathname', type=click.Path(exists=True))
+@click.argument('bucket')
+def sync(pathname, bucket):
+    "Sync contents of PATHNAME to BUCKET"
+    s3_bucket = s3.Bucket(bucket)
+
+    root = Path(pathname).expanduser().resolve()
+
+    def handle_directory(target):
+        for p in target.iterdir():
+            if p.is_dir():
+                handle_directory(p)
+            if p.is_file():
+                # print("Path: {} \n Key: {}".format(p, p.relative_to(root)))
+                relative_path = str(p.relative_to(root))
+                print("{0}: {1}".format(str(p), relative_path))
+                upload_file(s3_bucket, str(p), relative_path)
+
+    handle_directory(root)
 
 if __name__ == '__main__':
     cli()
